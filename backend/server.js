e5fd626428999947b23ff68aea002414cb0079ff
Skyler.tn <adem.arfaoui2025@gmail.com>
2026-06-22 11:42:18 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,8 @@ app.get("/health", (req, res) => {
 const PORT = 5000;
 
 connectDB();
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
